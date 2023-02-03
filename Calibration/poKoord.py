1264ea907ac22ed91ee3e681a7e5abb926cf24b5
Matej Mohorcic <mohori.matej@gmail.com>
2023-02-03 09:24:00 +0100

    poKoord.py move to calibration

--- Calibration/poKoord.py
+++ Calibration/poKoord.py
@@ -11,13 +11,19 @@ time.sleep(1)
 
 timeout = 2
 rspeed = 70
-rZ = -1
+squareSize = 19.18 # in mm
+rZ = -2
+
 while 1:
+    # wait for user input
+
     robot.set_position(x=0, y = 0,z = rZ, speed=rspeed, wait=True)
     time.sleep(timeout)
-    robot.set_position(x=150, y = 0,z = rZ, speed=rspeed, wait=True)
+    
+    robot.set_position(x=8*squareSize, y = 0,z = rZ, speed=rspeed, wait=True)
     time.sleep(timeout)
-    robot.set_position(x=150, y = 110,z = rZ, speed=rspeed, wait=True)
+    #input("Press Enter to continue...")
+    robot.set_position(x=8*squareSize, y = 8*squareSize,z = rZ, speed=rspeed, wait=True)
     time.sleep(timeout)
-    robot.set_position(x=0, y = 110,z = rZ, speed=rspeed, wait=True)
+    robot.set_position(x=0, y = 8*squareSize,z = rZ, speed=rspeed, wait=True)
     time.sleep(timeout)
