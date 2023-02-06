85f1964b78520e9380bec47296f3374b4d781a6e
Vid Urh <vidurh48@gmail.com>
2023-02-06 08:58:51 +0100

    Added virtual environment to git

---
+++ RobotVision/Scripts/miniterm.py
@@ -0,0 +1,4 @@
+#!C:\Users\vidur\OneDrive\Documents\Programiranja\RobotVision\RobotVision\Scripts\python.exe
+# EASY-INSTALL-SCRIPT: 'pyserial==3.4','miniterm.py'
+__requires__ = 'pyserial==3.4'
+__import__('pkg_resources').run_script('pyserial==3.4', 'miniterm.py')
