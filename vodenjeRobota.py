530163b71c1500a44e22e7b4509d114bd31953c3
Matej Mohorcic <mohori.matej@gmail.com>
2023-02-23 15:28:55 +0100

    vodenjeRobot.py: > update standby position > add new class which read stored worlsOffset > use this class in main

--- vodenjeRobota.py
+++ vodenjeRobota.py
@@ -28,7 +28,7 @@ from xarm.wrapper import XArmAPI
 
 
 class Robot:
-    def __init__(self, ip, logger):
+    def __init__(self, ip, logger, coordOffset=None):
         self.logger = logger
         self.logger.info("Connecting to robot...")
         self.robot = XArmAPI(ip)
@@ -37,6 +37,10 @@ class Robot:
         self.robot.set_mode(0)
         self.robot.set_state(state=0)
         self.logger.info("Robot connected!")
+        self.coordOffset = coordOffset
+
+        if coordOffset != None:
+            self.setWorldOffset(coordOffset)
 
     # move roboto set default parameters
     def move(self, x=None, y=None, z=None, roll=None, pitch=None, yaw=None, speed=SPEED_MIDDLE, mvacc=None, wait=True, timeout=0):
@@ -61,7 +65,7 @@ class Robot:
 
     def standby(self):
         self.robot.set_servo_angle(
-            angle=[90, 0, 90, 0.0, 90, 0], speed=SPEED_FAST, acceleration=5, is_radian=False, wait=True)
+            angle=[0, 0, 90, 0.0, 90, 0], speed=SPEED_FAST, acceleration=5, is_radian=False, wait=True)
 
     def getPosition(self):
         return self.robot.get_position()[1]
@@ -82,15 +86,36 @@ class Robot:
     def getIsMoving(self):
         return self.robot.get_is_moving()
 
-    def setWorldOffset(self, offset, is_radian=False):
+    def setWorldOffset(self, offset=None, is_radian=False):
+        '''
+        Če želimo uporabiti že shranjene skalibrirane world coordinate izberi worldCoordRobot class
+        in potem nekje v kodi naredi:
+        setWorldOffset()
+        
+        Dobro je dodati nekaj deleje za to nastavitvijo preden premaknemo robota.
+        '''
+        if offset == None:
+            offset = self.coordOffset
+        
         ret =  self.robot.set_world_offset(offset, is_radian)
         self.setState(0)
 
     def stop(self):
         self.setState(4)
 
+class worldCoordRobot(Robot):
+    '''
+    Uporabi kadar želiš nastaviti world coordinate offset iz shranjenih pickle datotek
+    '''
+    def __init__(self, ip, logger):
+        # read WORLD_COORDINATE_OFFSET_PATH file and set world offset
+        with open(WORLD_COORDINATE_OFFSET_PATH, 'rb') as f:
+            self.coordOffset = pickle.load(f)
+            print("World coordinate offset: ", self.coordOffset)
 
-if __name__ == "__main__":
+        Robot.__init__(self, ip, logger, self.coordOffset)
+
+def main():
     # Logging
     logging.basicConfig(
         level=logging.INFO,
@@ -103,8 +128,10 @@ if __name__ == "__main__":
     logger = logging.getLogger()
 
     # Robot
-    robot = Robot(ROBOT_IP, logger)
+    robot = worldCoordRobot(ROBOT_IP, logger)
     robot.home()
+
+    robot.setWorldOffset()
 
     # Detector
     detector = NozzleDetector()
@@ -144,3 +171,6 @@ if __name__ == "__main__":
                     logger.info("Object picked up and dropped!")
 
     robot.close()
+
+if __name__ == "__main__":
+    main()
